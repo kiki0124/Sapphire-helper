3ee6df08f23ec4fc2a214e5eb47f454ac56d2dbc
ShadowArcanist <162910371+ShadowArcanist@users.noreply.github.com>
2024-08-21 16:52:53 +0530

    Fix typo on the starter message (helper you faster -> help you faster)

--- cogs/autoadd.py
+++ cogs/autoadd.py
@@ -23,7 +23,7 @@ class autoadd(commands.Cog):
             await thread.edit(applied_tags=tags) # Add unanswered solved tag to post
             if len(thread.starter_message.content) < 15: # Check if the amount of characters in the starting message is smaller than 15 
                 greets = ["Hi", "Hey", "Hello", "Hi there"]
-                await thread.starter_message.reply(content=f"{random.choices(greets)[0]}, please answer these questions if you haven't already, so we can helper you faster.\n* What exactly is your question or the problem you're experiencing?\n* What have you already tried?\n* What are you trying to do / what is your overall goal?\n* If possible, please include a screenshot or screen recording of your setup.", mention_author=True)
+                await thread.starter_message.reply(content=f"{random.choices(greets)[0]}, please answer these questions if you haven't already, so we can help you faster.\n* What exactly is your question or the problem you're experiencing?\n* What have you already tried?\n* What are you trying to do / what is your overall goal?\n* If possible, please include a screenshot or screen recording of your setup.", mention_author=True)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
