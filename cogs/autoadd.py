1232281192b1ee111f340c6991ea44ec04a44fbb
kiki124 <128392944+kiki0124@users.noreply.github.com>
2024-09-09 21:31:52 +0300

    Add 'but' to negative regex

--- cogs/autoadd.py
+++ cogs/autoadd.py
@@ -36,7 +36,7 @@ class autoadd(commands.Cog):
                             solved_tag = message.channel.parent.get_tag(SOLVED_TAG_ID)
                             if solved_tag not in message.channel.applied_tags and need_dev_review_tag not in message.channel.applied_tags: # make sure the post is not already solved and doesn't have the need-dev-review tag
                                 if not message == message.channel.starter_message:
-                                    pattern = r'solved|^ty$|\sty|thank|work|fixed|thx'
+                                    pattern = r'solved|^ty$|\sty|thank|work|fixed|thx|but'
                                     negative_pattern = r"doesn'?t|isn'?t|not?"
                                     if not bool(re.search(negative_pattern, message.content, re.IGNORECASE)):
                                         if re.search(pattern, message.content, re.IGNORECASE):
