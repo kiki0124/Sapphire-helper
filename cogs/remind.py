cf64e9e8d3f4961043539cf0cf3ab708c5344e15
kiki124 <kiki1241245@gmail.com>
2024-11-11 10:23:06 +0200

    Remove waiting for reply system Will be recoded and readded later

--- cogs/remind.py
+++ cogs/remind.py
@@ -17,12 +17,11 @@ CUSTOM_BRANDING_TAG_ID = int(os.getenv("CUSTOM_BRANDING_TAG_ID"))
 MODERATORS_ROLE_ID = int(os.getenv("MODERATORS_ROLE_ID"))
 EXPERTS_ROLE_ID = int(os.getenv("EXPERTS_ROLE_ID"))
 ALERTS_THREAD_ID = int(os.getenv("ALERTS_THREAD_ID"))
-WAITING_FOR_REPLY_TAG_ID = int(os.getenv("WAITING_FOR_REPLY_TAG_ID"))
 NOT_SOLVED_TAG_ID = int(os.getenv("NOT_SOLVED_TAG_ID"))
 UNANSWERED_TAG_ID = int(os.getenv('UNANSWERED_TAG_ID'))
 
 reminder_not_sent_posts: dict[int, int] = {} # declare a dictionary of post ids: the amount of tries
-waiting_for_reply_posts: dict[int, asyncio.Task] = {} # declare a dictionary of post ids: the task for each post, used to know when to add the wiating for reply tag to posts
+#waiting_for_reply_posts: dict[int, asyncio.Task] = {} # declare a dictionary of post ids: the task for each post, used to know when to add the wiating for reply tag to posts
 
 
 class CloseNow(ui.View):
@@ -58,18 +57,6 @@ class remind(commands.Cog):
         self.send_reminders.cancel() # cancel the loop as the cog was unloaded
         self.close_pending_posts.cancel() # cancel the loop as the cog was unloaded
         self.check_exception_posts.cancel()
-
-    async def add_waiting_for_op_tag(post: discord.Thread):
-        unanswered = post.parent.get_tag(UNANSWERED_TAG_ID)
-        if not unanswered in post.applied_tags:
-            await asyncio.sleep(600)
-            tags = [post.parent.get_tag(NOT_SOLVED_TAG_ID), post.parent.get_tag(WAITING_FOR_REPLY_TAG_ID)]
-            if post.parent.get_tag(CUSTOM_BRANDING_TAG_ID) in post.applied_tags:
-                tags.append(post.parent.get_tag(CUSTOM_BRANDING_TAG_ID))
-            await post.edit(applied_tags=tags)
-            waiting_for_reply_posts.pop(post.id)
-        else:
-            return
 
     @tasks.loop(hours=1)
     async def check_exception_posts(self):
@@ -144,22 +131,6 @@ class remind(commands.Cog):
             if message.author == message.channel.owner:
                 remove_post_from_pending(message.channel.id) # Remove the message from pending list as the 
                 
-    async def waiting_for_reply_listener(self, message: discord.Message):
-        waiting_for_reply = message.channel.parent.get_tag(WAITING_FOR_REPLY_TAG_ID)
-        if waiting_for_reply in message.channel.applied_tags and message.author != message.channel.owner: # check if the post has waiting for reply tag and the author of the message is op
-            tags = [message.channel.parent.get_tag(NOT_SOLVED_TAG_ID)]
-            cb = message.channel.parent.get_tag(CUSTOM_BRANDING_TAG_ID)
-            if cb in message.channel.applied_tags:
-                    tags.append(cb) # add cb tag to the tags list
-            await message.channel.edit(applied_tags=tags) # edit the channel to remove waiting for reply tag
-        if waiting_for_reply not in message.channel.applied_tags and message.author == message.channel.owner: # check if the post doesn't have waiting for reply tag and the message author isn't op
-            task = asyncio.create_task(self.AddWaitingForOpTag(message.channel)) # create a new task with the AddWaitingForOpTag coroutine
-            waiting_for_reply_posts[message.channel.id] = task # add the task with the channel to the dict
-        if waiting_for_reply not in message.channel.applied_tags and message.author != message.channel.owner and message.channel.id in waiting_for_reply_posts.keys():
-            waiting_for_reply_posts.pop(message.channel.id) # remove the channel from the list
-        else:
-            return # any other scenario, ignore the message
-        
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if not message.author == self.client.user:
@@ -167,10 +138,6 @@ class remind(commands.Cog):
                 if message.channel.parent_id == SUPPORT_CHANNEL_ID: # check if the thread's parent is #support
                     need_dev_review = message.channel.parent.get_tag(NEED_DEV_REVIEW_TAG_ID)
                     if not message.channel.locked and not need_dev_review in message.channel.applied_tags: # check if the post doesn't have ndr and isn't locked
-                        unanswered = message.channel.parent.get_tag(UNANSWERED_TAG_ID)
-                        solved = message.channel.parent.get_tag(SOLVED_TAG_ID)
-                        if not unanswered in message.channel.applied_tags and not solved in message.channel.applied_tags:
-                            await self.waiting_for_reply_listener(message) # call the AddRmoveWaitingTag coroutine to add/remove the waiting for op tag based on the need
                         await self.pending_posts_listener(message) # call the PendingPostsListener coroutien that is related to the reminder system
 
     @tasks.loop(hours=1)
