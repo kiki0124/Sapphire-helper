60917dd32146e8bf3007bef552cc348dc0efb826
kiki124 <kiki1241245@gmail.com>
2025-01-07 15:42:35 +0200

    Slightly change the way a title is generated for rtdr post

--- cogs/readthedamnrules.py
+++ cogs/readthedamnrules.py
@@ -40,8 +40,7 @@ class readthedamnrules(commands.Cog):
         files = await self.get_files(messages_to_move)
         content = '\n'.join([msg.content for msg in messages_to_move])
         support = self.client.get_channel(SUPPORT_CHANNEL_ID)
-        title = f"Support for {reference_message.author.name}"
-        if message: title = message.content.removeprefix(self.client.user.mention)
+        title = message.content.removeprefix(self.client.user.mention) or f"Support for {reference_message.author.name}"
         post_data = await support.create_thread(
             name=title,
             files=files,
