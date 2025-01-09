192732e36e86c52adb0bab7be4b37a95661c90fe
kiki124 <kiki1241245@gmail.com>
2025-01-09 07:42:04 +0200

    Add system to delete accidental QRs

--- cogs/utility.py
+++ cogs/utility.py
@@ -7,6 +7,7 @@ import os
 from dotenv import load_dotenv
 from functions import remove_post_from_rtdr, get_post_creator_id, generate_random_id
 from aiocache import cached
+from typing import Union
 
 load_dotenv()
 
@@ -227,6 +228,22 @@ class utility(commands.Cog):
             else:
                 await interaction.response.send_message(content="This post already has needs-dev-review tag.", ephemeral=True)
             
+    @commands.Cog.listener('on_reaction_add')
+    async def delete_accidental_qr(self, reaction: discord.Reaction, user: Union[discord.Member, discord.User]):
+        in_support = isinstance(reaction.message.channel, discord.Thread) \
+            and reaction.message.channel.parent_id == SUPPORT_CHANNEL_ID
+        from_sapphire = reaction.message.author.id == 678344927997853742 # Sapphire's user id
+        allowed_reactions = ["🗑️", "❌"]
+        reaction_allowed = reaction.emoji in allowed_reactions
+        if in_support and from_sapphire and reaction_allowed:
+            experts = reaction.message.guild.get_role(EXPERTS_ROLE_ID)
+            if experts in user.roles:
+                await reaction.message.delete()
+            else:
+                if f"@{user.name}" in reaction.message.embeds[len(reaction.message.embeds)-1].footer.text: # check if @user_name is in the footer of the last embed
+                    await reaction.message.delete()
+
+
     @get_tags.before_loop
     async def wait_until_ready(self):
         await self.client.wait_until_ready()
