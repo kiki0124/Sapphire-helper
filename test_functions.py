51c40af0e1a3af18e47cb8e45d983e670e865daf
kiki124 <kiki1241245@gmail.com>
2025-01-10 19:42:11 +0200

    Remove inaccurate comment

--- test_functions.py
+++ test_functions.py
@@ -7,7 +7,7 @@ class test_functions(unittest.TestCase):
     tz = datetime.now().astimezone().tzinfo
 
     def test_less_than_day(self):
-        time_object = datetime.now(tz=self.tz) - timedelta(hours=5) # 6 hours behind now
+        time_object = datetime.now(tz=self.tz) - timedelta(hours=5)
         self.assertFalse(check_time_more_than_day(time_object.timestamp()))
     
     def test_more_than_day(self):
