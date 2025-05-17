9cb63ccebb59afde096509ad47529699e922bb31
kiki124 <kiki1241245@gmail.com>
2025-05-17 09:48:24 +0300

    Change version in sh!stats to 3.4

--- cogs/bot.py
+++ cogs/bot.py
@@ -103,7 +103,7 @@ class bot(commands.Cog):
     @commands.has_any_role(EXPERTS_ROLE_ID, MODERATORS_ROLE_ID)
     async def stats(self, ctx: commands.Context):
         embed = discord.Embed(
-            title="Sapphire Helper | Version 3.3",
+            title="Sapphire Helper | Version 3.4",
             colour=discord.Colour.purple()
         )
         embed.add_field(name="CPU Count:", value=os.cpu_count(), inline=False)
