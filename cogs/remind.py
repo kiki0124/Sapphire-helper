c74eadd8488d294a4a50f59b63a362bdad12e6d9
kiki124 <kiki1241245@gmail.com>
2025-08-16 09:18:17 +0300

    Change random.choices to random.choice and remove indexing

--- cogs/remind.py
+++ cogs/remind.py
@@ -116,7 +116,7 @@ class remind(commands.Cog):
                 if check_time_more_than_day(message.created_at.timestamp()):
                     if post.owner: # make sure the post owner is not None- still in server
                         greetings = ["Hi", "Hello", "Hey", "Hi there"]
-                        await message.channel.send(content=f"{random.choices(greetings)[0]} {post.owner.mention}, it seems like your last message was sent more than 24 hours ago.\nIf we don't hear back from you we'll assume the issue is resolved and mark your post as solved.", view=CloseNow())
+                        await message.channel.send(content=f"{random.choice(greetings)} {post.owner.mention}, it seems like your last message was sent more than 24 hours ago.\nIf we don't hear back from you we'll assume the issue is resolved and mark your post as solved.", view=CloseNow())
                         await add_post_to_pending(post_id=post.id)
                         to_remove.append(post.id)
                         continue
@@ -172,7 +172,7 @@ class remind(commands.Cog):
                             if post.owner: # make sure post owner isn't None- still in server
                                 greetings = ["Hi", "Hello", "Hey", "Hi there"]
                                 post_author_id = await get_post_creator_id(post.id) or post.owner_id
-                                await message.channel.send(content=f"{random.choices(greetings)[0]} <@{post_author_id}>, it seems like your last message was sent more than 24 hours ago.\nIf we don't hear back from you we'll assume the issue is resolved and mark your post as solved.", view=CloseNow())
+                                await message.channel.send(content=f"{random.choice(greetings)} <@{post_author_id}>, it seems like your last message was sent more than 24 hours ago.\nIf we don't hear back from you we'll assume the issue is resolved and mark your post as solved.", view=CloseNow())
                                 await add_post_to_pending(post_id=post.id)
             
     @commands.Cog.listener('on_message')
