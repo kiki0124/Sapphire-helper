e9bfab319239b3515af8b0c088bfae5c197f93b5
kiki124 <kiki1241245@gmail.com>
2026-01-17 10:49:47 +0200

    Update version to 6.0

--- cogs/bot.py
+++ cogs/bot.py
@@ -106,7 +106,7 @@ class bot(commands.Cog):
     @commands.has_any_role(EXPERTS_ROLE_ID, MODERATORS_ROLE_ID, DEVELOPERS_ROLE_ID)
     async def stats(self, ctx: commands.Context):
         embed = discord.Embed(
-            title="Sapphire Helper | Version 5.0",
+            title="Sapphire Helper | Version 6.0",
             colour=discord.Colour.purple(),
             url="https://github.com/kiki0124/sapphire-helper"
         )
