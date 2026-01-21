d2fb5e0d2d7c6eba9819b441f83c6c98a0240fa9
naemless <naemless5@gmail.com>
2026-01-21 12:38:42 +0200

    Fix typo in stats command

--- SH/cogs/bot.py
+++ SH/cogs/bot.py
@@ -115,7 +115,7 @@ class bot(commands.Cog):
                      f"**CPU Load:** {psutil.cpu_percent()}%\n"
                      f"**Available memory:** {str(round(psutil.virtual_memory()[0]/1000000000))}GB\n"
                      f"**Memory Usage:** {psutil.virtual_memory()[2]}%\n"
-                     f"-# discord.py vesion {discord.__version__}")
+                     f"-# discord.py version {discord.__version__}")
         
         container.add_item(title)
         container.add_item(ui.Separator(spacing=discord.SeparatorSpacing.large))
