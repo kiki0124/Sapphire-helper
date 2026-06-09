1d523836286d9d8f2bce178f46b06c46420ff5c1
Sacul0457Deve <183588943+Sacul0457@users.noreply.github.com.>
2026-06-10 01:33:57 +0800

    remove white spaces

--- SH/cogs/autoadd.py
+++ SH/cogs/autoadd.py
@@ -140,7 +140,7 @@ class autoadd(commands.Cog):
             container.add_item(
                 ui.TextDisplay(f"{random.choice(greets)}, please answer these questions if you haven't already, so we can help you faster.\n* What exactly is your question or the problem you're experiencing?\n* What have you already tried?\n* What are you trying to do / what is your overall goal?\n* If possible, please include a screenshot or screen recording of your setup.")
             )
-            await thread.starter_message.reply(view=view, mention_author=True)  
+            await thread.starter_message.reply(view=view, mention_author=True)
             self.client.incomplete_msg_posts.add(thread.id)
 
     async def send_suggestion_message(self, message: discord.Message):
