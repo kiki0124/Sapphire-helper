17cbf1cdc0915640b0349257ed2595533f87eef1
Sacul0457Deve <183588943+Sacul0457@users.noreply.github.com.>
2026-09-17 18:05:37 +0800

    chore: don't return anything in hande_request RTDR

--- SH/cogs/readthedamnrules.py
+++ SH/cogs/readthedamnrules.py
@@ -92,7 +92,7 @@ class RTDR(commands.Cog):
         else:
             return f"{reference_msg.author.mention} please provide any additional information here so we can give you the best help." # default message - if none of the special ones above are used
 
-    async def handle_request(self, reference_message: discord.Message, user: discord.Member, message: discord.Message|None = None) -> discord.Thread:
+    async def handle_request(self, reference_message: discord.Message, user: discord.Member, message: discord.Message|None = None) -> None:
         await reference_message.channel.typing()
         messages_to_move: list[discord.Message] = await self.get_messages_to_move(reference_message)
         gallery_items, files = await self.get_media_gallery_items(messages_to_move)
@@ -121,8 +121,7 @@ class RTDR(commands.Cog):
         )
         self.bot.add_post_to_rtdr(thread_id=post[0].id, owner_id=reference_message.author.id)
         await reference_message.channel.send(content=f'{reference_message.author.mention} asked something about Sapphire or appeal.gg. A post was opened to answer it: {post[0].mention}\n-# Please ask any Sapphire or appeal.gg related questions in <#{SUPPORT_CHANNEL_ID}>. Asking anywhere else repeatedly will result in a punishment.', delete_after=300, allowed_mentions=discord.AllowedMentions.none())
-        await reference_message.channel.delete_messages(messages_to_move, reason=f"rtdr system used by {user.name}")
-        return post[0]
+        await reference_message.channel.delete_messages(messages_to_move, reason=f"rtdr system used by {user.name}")\
     
     @staticmethod
     def is_allowed_rtdr_channel(channel: discord.TextChannel | discord.Thread, everyone_role: discord.Role):
